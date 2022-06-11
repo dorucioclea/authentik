9438b9f7936c27fdd22ba9e7e25a49db188c7ac4
Jens Langhammer <jens.langhammer@beryju.org>
2022-06-11 23:59:57 +0200

    antlr for scim filter parsing, why
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/sidebarsIntegrations.js
+++ website/sidebarsIntegrations.js
@@ -173,6 +173,7 @@ module.exports = {
                         "sources/ldap/index",
                         "sources/oauth/index",
                         "sources/saml/index",
+                        "sources/scim/index",
                     ],
                 },
                 {
